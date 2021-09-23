d6f49904a6cdf7329701d2f5e3e34a78e77d600e
danceoval <daniel.sohval@gmail.com>
2021-09-23 11:03:54 -0400

    adds form demo

--- mod-1/react-forms/public/react/components/Cart.js
+++ mod-1/react-forms/public/react/components/Cart.js
@@ -2,7 +2,33 @@ import React, {useState} from 'react';
 
 
 export const Cart = (props) => {
-	console.log("cart in props ", props.cart)
+	
+	const [card, setCard] = useState({type : '', ccn : ''});
+	const [message, setMessage] = useState('')
+
+	function handleCCN(e){
+		// console.log("ccn change event target", e.target.value)
+
+		const updatedCard = card;
+		updatedCard.ccn = e.target.value
+
+		setCard(updatedCard)
+		// console.log("CARD ON STATE", card)
+	}
+
+	function handleType(e){
+		const updatedCard = card;
+		updatedCard.type = e.target.value
+		setCard(updatedCard)
+
+		console.log("PAYMENT TYPE ", card)
+	}
+
+	function handleSubmit(e){
+		e.preventDefault() // prevents refresh in all browsers
+		setMessage(`PAYMENT SUBMITTED TO ${card.type} #${card.ccn}`)
+	}
+
 	return(
 		<div>
 			Your Cart:
@@ -12,6 +38,29 @@ export const Cart = (props) => {
 					return <div key={idx}><strong>{itemName}: </strong>{props.cart[itemName]}</div>
 				})
 			}
+			<br />
+			{
+				message ? message : (
+					<form onSubmit={handleSubmit}>
+						<div>
+							<label>CCN: </label>
+							<input type="text" name="ccn" onChange={handleCCN} />
+						</div>
+						< br />
+						<div>
+							<label>Payment Type: </label>
+							<select onChange={handleType}>
+								<option></option>
+								<option>Visa</option>
+								<option>Amex</option>
+								<option>Bitcoin</option>
+							</select>
+						</div>
+						<button type="submit" className="btn">Submit Order</button>
+					</form>
+				)
+			}
+			
 		</div>
 	)
 }
